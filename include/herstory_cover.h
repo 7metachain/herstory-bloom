600f8d056a38c6cc53d59800a92154c3700beff4
7metachain <7metachain@users.noreply.github.com>
2026-07-26 12:04:51 +0800

    Add boot cover image resource

---
+++ include/herstory_cover.h
@@ -0,0 +1,9 @@
+#ifndef HERSTORY_COVER_H
+#define HERSTORY_COVER_H
+
+#include <stdint.h>
+
+extern const unsigned char herstory_cover_jpeg[];
+extern const unsigned int herstory_cover_jpeg_len;
+
+#endif
